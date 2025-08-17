8e572396df46cbe419a957a83db5d38f89715ffa
olsiflime99 <olsiflime99@gmail.com>
2025-08-17 17:16:33 +0700

    ghj

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 
+/* hw2 */
 /* Задание 1 */
-let a = 10;
-alert(a);
-a = alert (20);
+let password = String(prompt('Введите пароль'));
+/*password = password.toLocaleLowerCase;   Почему то не работает */
+switch (password) {
+    case 'пароль':
+    console.log('Пароль введен верно')
+        break;
+    default:
+        console.log('Пароль введен не верно')
+        break;
+}
+
+
 /* Задание 2 */
-let iPhone = 2007;
-alert (iPhone);
+
 /* Задание 3 */
-let nameJS = 'Брендан Айк'
-alert (nameJS);
+
 /* Задание 4 */
-let z = 10;
-let x = 2;
-alert (z+x);
-alert (z-x);
-alert (z*x);
-alert (z/x);
+
 /* Задание 5 */
-let result = 2;
-alert (result**5)
-/* Задание 6 
-let a = 9;
-let b = 2;
-let modyl = (a % b );
-alert = modyl;       
-*/
-/* Задание 7 */
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
-/* Задание 8 */
-let age = prompt('Сколько вам лет?');
-alert (age);
-/* Задание 9 
-let name = String("Ник");
-let age = Number(17);
-let isAdmin = Boolean (false);*/
-/* Задание 10 */
-let YouName = prompt('Как ваше имя?');
-alert(`Привет, ${YouName}`);  
 
