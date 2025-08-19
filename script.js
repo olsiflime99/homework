d3296da1fe51020c08827fb7c90e76ffd1a4cc34
olsiflime99 <olsiflime99@gmail.com>
2025-08-19 20:26:06 +0700

    JS+шрифт

--- script.js
+++ script.js
@@ -33,42 +33,26 @@ alert(l);
 let monthNumber = String(prompt('Напиши цифру месяца'));
 switch (monthNumber) {
     case '1':
-        console.log('Январь');
-        break;
     case '2':
-        console.log('Февраль');
+    case '12':        
+        console.log('Зима');
         break;
     case '3':
-        console.log('Март');
-        break;
     case '4':
-        console.log('Апрель');
-        break;
     case '5':
-        console.log('Май');
+        console.log('Весна');
         break;
     case '6':
-        console.log('Июнь');
-        break;
     case '7':
-        console.log('Июль');
-        break;
     case '8':
-        console.log('Август');
+        console.log('Лето');
         break;
     case '9':
-        console.log('Сентябрь');
-        break;
     case '10':
-        console.log('Октябрь');
-        break;
     case '11':
-        console.log('Ноябрь');
+        console.log('Осень');
         break;
-    case '12':
-        console.log('Декабрь');
-        break;
-    default:
+   default:
         console.log('Такого месяца нет');
         break;
 }
